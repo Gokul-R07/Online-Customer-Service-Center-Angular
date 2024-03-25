ff1935b494cda83e6d88501c449dfd7cdc722c6b
yashwanth-AK <yashwanthak091@gmail.com>
2024-03-25 17:46:01 +0530

    Final commit before adding the previous solution

--- src/app/operator/components/operator-module/operator-module.component.ts
+++ src/app/operator/components/operator-module/operator-module.component.ts
@@ -21,62 +21,86 @@ export class OperatorModuleComponent implements OnInit {
   data!:[];
   operator!:any;
   operatorDetails: any;
-  operatorId:string = '1';
+  operatorId:any;
   response: any;
   cols!:Column[];
+  lastSolution!:string;
+  
+
+
   constructor(private dataService: DataService,private route: ActivatedRoute,private router: Router, private http:HttpClient) { 
+
+     // state to reference of the operator.
+    // this logic is to be reviewed.
     const navigation = this.router.getCurrentNavigation();
-if (navigation?.extras.state) {
-this.operatorDetails = navigation.extras.state['activeOperatorDetails'];
-console.log('Operator Details:', this.operatorDetails);
-}
+    
+  if (navigation?.extras.state) {
+      this.operatorDetails = navigation.extras.state['activeOperatorDetails'];
+      console.log('Operator Details:', this.operatorDetails);
+      this.operatorId = this.operatorDetails.operatorId
+
+      localStorage.setItem('Id',this.operatorId);
   }
 
-  loading: boolean = false;
+}
 
+
+
+  // logout and move to the login page again.
   logout(){
     localStorage.clear();
     this.router.navigate(['/home']);
   }
 
-
-  navToSolution(issueDescription:string,){
-    this.router.navigate(['/solution',{description:issueDescription}]);
+// navigation to the solution module.
+  navToSolution(issueDescription:string,issueId:number){
+    // this.router.navigate(['/solution',{description:issueDescription}]);
+    
+    this.router.navigate(['/solution'],{state:{description: issueDescription,Id: issueId,operatorId : localStorage.getItem('Id')}});
   }
 
   ngOnInit(): void {
+
+    // this.data= this.operatorDetails.customerIssues;
     
 
 
 // get all allocated issue by  id
 
 
-// this.http.get('http://localhost:8080/allocated-issue-by-id/'+ this.operatorId).subscribe(
-//   (response: any)=>{
-//     this.data = response;
-//     console.log(response);
-//   },
-//   (error)=>{
-//     console.log('Error fetching allocated issue',error);
-//   }
-// )
+this.http.get('http://localhost:8080/allocated-issue-by-id/'+ localStorage.getItem('Id')).subscribe(
+  (response: any)=>{
+    
+    this.data = response;
+    console.log(response);
+
+    const solution = response.solutions;
+    console.log(solution);
+    localStorage.setItem('solutions',solution);
+    console.log(localStorage.getItem('solutions'));
+    
+  },
+  (error)=>{
+    console.log('Error fetching allocated issue',error);
+  }
+)
 
 
 
 // get all allocated issue 
 
-this.http.get('http://localhost:8080/Allocated-issue').subscribe(
-  (response:any)=>{
-    this.data = response;
-    console.log(response);
-  },
-  (error)=>{
-    console.error('Allocated issue error'+error);
-  }
-)
+// this.http.get('http://localhost:8080/Allocated-issue').subscribe(
+//   (response:any)=>{
+//     this.data = response;
+//     console.log(response);
+//   },
+//   (error)=>{
+//     console.error('Allocated issue error'+error);
+//   }
+// )
 
 
-  
+  // defining the columns 
   this.cols=[
 
     {field:"issueId",header:"Issue Id"},
--- src/app/solution-module/solution-module.component.ts
+++ src/app/solution-module/solution-module.component.ts
@@ -6,49 +6,103 @@ import { SolutionModuleServiceService } from './service/solution-module-service.
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
+
+
 @Component({
   selector: 'app-solution-module',
   templateUrl: './solution-module.component.html',
   styleUrls: ['./solution-module.component.css']
 })
+
+
 export class SolutionModuleComponent implements OnInit {
+
+
+
+// code to navigate back to the issue table module.
 goBack() {
-this.router.navigate(['/operator']);
+this.router.navigate(['/operator'],);
 };
+
+
+
   formGroup!: FormGroup;
-  issueDescription: string | undefined;
-  issueId:number|undefined;
+ 
+  issueId:any;
+  operatorId:any;
 
   issueType = ["Software", "Hardware"];
   solutionText!: string;
-  previousSolution = "No previous solution provided";
+  previousSolution:any;
+  operatorDetails:any;
+  issueDescription!: string;
+  currentIssueDescription!:any;
+  
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private toastr: ToastrService) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private toastr: ToastrService) {
+
+    
+
+    
+      const navigation = this.router.getCurrentNavigation();
+      console.log(navigation); // Log the navigation object
+  
+      if (navigation && navigation.extras.state) {
+        
+        console.log(navigation.extras.state); // Log the state object
+        this.issueDescription = navigation.extras.state['description'];
+        this.issueId = navigation.extras.state['Id'];
+        this.operatorId = navigation.extras.state['operatorId'];
+        console.log(this.issueDescription);
+        console.log(this.issueId);
+        console.log(this.operatorId);
+        localStorage.setItem('description',this.issueDescription);
+        localStorage.setItem('Id',this.issueId);
+        localStorage.setItem('operatorId',this.operatorId);
+      }
+      
+  }
 
   ngOnInit() {
+
+    this.currentIssueDescription = localStorage.getItem('description');
+
     this.formGroup = new FormGroup({
       text: new FormControl<string | null>(null)
     });
 
     this.route.params.subscribe(params => {
+
+      // code to get the issue description 
       this.issueDescription = params['description'];
+      console.log(this.issueDescription);
      
     });
+
+  
   }
 
   onSubmit() {
+
+    // the request body to send the required value as text
     const requestBody = {
       
-        "issueId": 1,
+        "issueId": localStorage.getItem('Id'),
         "solutionDescription": this.solutionText,
-        "operatorId": 1
+        "operatorId": localStorage.getItem('operatorId')
       
     };
 
+
+    // process of checking whether the text area contains just empty spaces or not.
     
     if (this.solutionText == "") {
+
       this.toastr.error('Solution cannot be empty!')
-    } else {
+    } 
+    
+    // working with the post mapping 
+    else {
       this.http.post(`http://localhost:8080/operator/solution`, requestBody,{responseType:'text'})
       .subscribe(
       
@@ -56,14 +110,28 @@ this.router.navigate(['/operator']);
           
           console.log('Solution submitted successfully');
           this.toastr.success('Solution submitted successfully');
+          localStorage.setItem('solution',this.solutionText);
           this.solutionText="";
         },
+
+        // handling the error of past mapping 
         err => {
           console.error('Failed to submit solution', err);
           this.toastr.error('Failed to submit solution');
         }
       );
     }
-    this.goBack();
+
+    // setting the time to show the toast and come back to the issue page 
+    setTimeout(()=>{
+
+      this.goBack(); 
+
+    },1000); // 1 sec delay in this process.
+
+    
+    
   }
+
+
 }
